1acbaa70f83be8f0aff114d4380e946f26547bc6
Pedro Melo <melo@simplicidade.org>
2012-07-26 17:45:55 +0100

    Move current implementation of Logger to a Role:
    This will allow apps to rebuild a Logger class with just the features they
    need by creating a new logger class and mixin the roles they need.
    
    L::L::Logger::Core is the essential set of attributes and roles that
    every Logger class needs.
    
    L::L::Logger is our "best" logger with a growing set of features.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Log/Logan/Logger.pm
+++ lib/Log/Logan/Logger.pm
@@ -5,25 +5,6 @@ package Log::Logan::Logger;
 # AUTHORITY
 
 use Moo;
-
-has 'logan' => (is => 'ro', required => 1, weak_ref => 1);
-has 'id' => (is => 'lazy');
-
-sub _build_id { shift->logan->generate_id }
-
-sub event {
-  my ($self, $class, $subclass, $msg, $user_data) = @_;
-  $msg       = '' unless defined $msg;
-  $user_data = {} unless ref $user_data;
-
-  my $event = {
-    class    => $class,
-    subclass => $subclass,
-    msg      => $msg,
-    data     => $user_data,
-  };
-
-  return $self->logan->process($event);
-}
+with 'Log::Logan::Logger::Core';
 
 1;
---
+++ lib/Log/Logan/Logger/Core.pm
@@ -0,0 +1,25 @@
+package Log::Logan::Logger::Core;
+
+use Moo::Role;
+
+has 'logan' => (is => 'ro', required => 1, weak_ref => 1);
+has 'id' => (is => 'lazy');
+
+sub _build_id { shift->logan->generate_id }
+
+sub event {
+  my ($self, $class, $subclass, $msg, $user_data) = @_;
+  $msg       = '' unless defined $msg;
+  $user_data = {} unless ref $user_data;
+
+  my $event = {
+    class    => $class,
+    subclass => $subclass,
+    msg      => $msg,
+    data     => $user_data,
+  };
+
+  return $self->logan->process($event);
+}
+
+1;
