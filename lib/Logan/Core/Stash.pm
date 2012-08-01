ec7331886a042d1795455c5fb475a7160de392e1
Pedro Melo <melo@simplicidade.org>
2012-08-01 18:38:50 +0100

    Move Core::Stash implementation to a Roles::Stash for reuse
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Logan/Core/Stash.pm
+++ lib/Logan/Core/Stash.pm
@@ -3,31 +3,7 @@ package Logan::Core::Stash;
 use Moo::Role;
 use namespace::autoclean;
 
-
 ### Stash for session components use
-### FIXME: hate the name stash... can't think of a better one right now
-has '_stash' => (is => 'ro', default => sub { {} });
-
-sub stash_for {
-  my $self  = shift;
-  my $stash = $self->_stash;
-
-  my $key = shift;
-  $key = join('  --  ', @$key) if ref $key;
-
-  return $stash->{$key} = shift if @_;
-  return $stash->{$key} if exists $stash->{$key};
-  return;
-}
-
-sub stash {
-  my $self = shift;
-  my $key  = shift;
-  my $ns   = caller();
-
-  $key = [$ns, @{ ref $key ? $key : [$key] }];
-
-  return $self->stash_for($key, @_);
-}
+with 'Logan::Roles::Stash';
 
 1;
