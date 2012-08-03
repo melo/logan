ad3b51f60ad872b819edd582fb3f33bae519813b
Pedro Melo <melo@simplicidade.org>
2012-08-03 23:10:05 +0100

    Use our Config::Attribute role on the MessagePassing test Logan class
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- t/tlib/T/MessagePassing/Logan.pm
+++ t/tlib/T/MessagePassing/Logan.pm
@@ -3,7 +3,7 @@ package T::MessagePassing::Logan;
 use Moo;
 use Message::Passing::Output::Test;
 extends 'Logan';
-with 'Logan::Dispatch::Message::Passing';
+with 'Logan::Config::Attribute', 'Logan::Dispatch::Message::Passing';
 
 sub default_no_filter_should_dispatch {1}
 sub build_message_passing_output  { Message::Passing::Output::Test->new }
