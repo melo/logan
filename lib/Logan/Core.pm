34967cd35d2973b986445b832aef40e29eff98b8
Pedro Melo <melo@simplicidade.org>
2012-08-03 16:23:57 +0100

    Added Core::Config to Core: define API signal_config_updated
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Logan/Core.pm
+++ lib/Logan/Core.pm
@@ -4,7 +4,9 @@ use Moo::Role;
 use Logan::Session;
 use namespace::autoclean;
 
-with 'Logan::Core::Singleton',
+with
+  'Logan::Core::Config',
+  'Logan::Core::Singleton',
   'Logan::Core::SessionFactory',
   'Logan::Core::Filter',
   'Logan::Core::EventProcessing',
---
+++ lib/Logan/Core/Config.pm
@@ -0,0 +1,8 @@
+package Logan::Core::Config;
+
+use Moo::Role;
+use namespace::autoclean;
+
+sub signal_config_updated { }
+
+1;
