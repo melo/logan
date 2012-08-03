0f2be3fec9de5f5ee5ec5c9ab3eadf6eac30e113
Pedro Melo <melo@simplicidade.org>
2012-08-03 17:09:35 +0100

    Add logs to all decisions while filtering an event
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Logan/Core/EventProcessing.pm
+++ lib/Logan/Core/EventProcessing.pm
@@ -3,12 +3,19 @@ package Logan::Core::EventProcessing;
 use Moo::Role;
 use namespace::autoclean;
 
+#requires 'match_rules', 'dispatch';
 requires 'match_rules';
 
 sub process {
   my ($self, $session, $event) = @_;
 
-  return unless $self->match_rules($session, $event);
+  ## return unless $self->match_rules($session, $event);
+  use Data::Dump qw(pp);
+  push @{ $event->{m}{log} }, "## filter event ", pp($event);
+  my $matched = $self->match_rules($session, $event);
+  push @{ $event->{m}{log} }, "## matched result is $matched";
+  return unless $matched;
+  push @{ $event->{m}{log} }, "## dispatching...";
 
   ## FIXME: make sure all enrichment callbacks are executed
 
--- lib/Logan/Core/Filter.pm
+++ lib/Logan/Core/Filter.pm
@@ -49,14 +49,20 @@ sub match_rules {
 
   ## Allow per-event override
   return 0 if exists $event->{'m'}{force_discard} and $event->{'m'}{force_discard};
+  push @{ $event->{m}{log} }, "## ... still here after force_discard";
   return 1 if exists $event->{'m'}{force_dispatch} and $event->{'m'}{force_dispatch};
+  push @{ $event->{m}{log} }, "## ... still here after force_dispatch";
 
   ## No filter, no dispatch?
   my $f_sub = $self->_filter_sub;
+  push @{ $event->{m}{log} },
+    "## ... no filter, will exit with " . $self->no_filter_should_dispatch . " via no_filter_should_dispatch"
+    unless ref($f_sub) eq 'CODE';
 
   return $self->no_filter_should_dispatch unless ref($f_sub) eq 'CODE';
 
   ## Run the rules gauntlet
+  push @{ $event->{m}{log} }, "##  ... running filter";
   return $f_sub->($self, $session, $event);
 }
 
