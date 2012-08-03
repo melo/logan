c3bd1819ad411346352031ea884b6b8674c72c1b
Pedro Melo <melo@simplicidade.org>
2012-08-03 17:26:24 +0100

    L::D::Message:Passing: renamed build_message_passing_dispatcher to _output
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- t/tlib/T/MessagePassing/Logan.pm
+++ t/tlib/T/MessagePassing/Logan.pm
@@ -6,6 +6,6 @@ extends 'Logan';
 with 'Logan::Dispatch::Message::Passing';
 
 sub default_no_filter_should_dispatch {1}
-sub build_message_passing_dispatcher  { Message::Passing::Output::Test->new }
+sub build_message_passing_output  { Message::Passing::Output::Test->new }
 
 1;
