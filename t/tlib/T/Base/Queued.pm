f45d89d9ddadd8d7c0b96deb823b111b0bf4f46b
Pedro Melo <melo@simplicidade.org>
2012-08-03 16:45:33 +0100

    Move common default_no_filter_should_dispatch to T::Base::Queued
    We need to force it to 0 (back to the default value) on T::Filter::Logan,
    to keep filter tests happy.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- t/tlib/T/Base/Queued.pm
+++ t/tlib/T/Base/Queued.pm
@@ -4,4 +4,6 @@ use Moo;
 extends 'Logan';
 with 'Logan::Config::Attribute', 'T::Dispatch::Queue';
 
+sub default_no_filter_should_dispatch {1}
+
 1;
--- t/tlib/T/MetaCache/Logan.pm
+++ t/tlib/T/MetaCache/Logan.pm
@@ -5,7 +5,6 @@ use T::MetaCache::Logger;
 
 extends 'T::Base::Queued';
 
-sub default_no_filter_should_dispatch {1}
-sub default_session_class             {'T::MetaCache::Logger'}
+sub default_session_class {'T::MetaCache::Logger'}
 
 1;
