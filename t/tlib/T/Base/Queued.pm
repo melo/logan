9184e9b90f9d1cb52765b9667e6832af252826d4
Pedro Melo <melo@simplicidade.org>
2012-08-03 16:20:38 +0100

    Add Logan::Config::Static to T::Base::Queued
    All Logan's require a config now. Remove it from the only subclass that
    used it directly, T::Config::Static.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- t/tlib/T/Base/Queued.pm
+++ t/tlib/T/Base/Queued.pm
@@ -2,6 +2,6 @@ package T::Base::Queued;
 
 use Moo;
 extends 'Logan';
-with 'T::Dispatch::Queue';
+with 'Logan::Config::Static', 'T::Dispatch::Queue';
 
 1;
--- t/tlib/T/Config/Static.pm
+++ t/tlib/T/Config/Static.pm
@@ -2,7 +2,6 @@ package T::Config::Static;
 
 use Moo;
 extends 'T::Base::Queued';
-with 'Logan::Config::Static';
 
 sub default_no_filter_should_dispatch {1}
 
